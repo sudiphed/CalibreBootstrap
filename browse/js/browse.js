8e87ca1a3dec31d475399bd28911983aadf63f12
Craig <craig@sandcube.net>
2014-01-09 14:59:05 -0600

    Modal works and some other cleanup

--- browse/js/browse.js
+++ browse/js/browse.js
@@ -1,3 +1,35 @@
+$(document).ready(function() {
+    eval(script);
+    init();
+
+    // Selectors
+    var $sort_combobox_container = $("#sort_combobox_container"),
+$sort_combobox = $("#sort_combobox");
+
+// Beautify our buttons
+$(".ui-button").button().addClass("btn");
+$(".navlink, .navleft a, .navright a").button().addClass("btn btn-primary btn-small");
+
+// Goto the first page
+goto_page();
+
+// I forget what this does.
+if (window.location.href.indexOf('category') !== -1) {
+    if (window.location.href.indexOf('newest') === -1) {
+        $sort_combobox_container.removeClass("hide");
+    }
+}
+
+// When our sort order changes, resort and reload
+$sort_combobox.on("change",function() {
+    cookie(sort_cookie_name, this.value);
+    window.location.reload();
+});
+
+$(document).on("click","a.details",function() {
+    show_details(this);
+});
+
 
 // Cookies {{{
 /**
@@ -78,29 +110,26 @@ function cookie(name, value, options) {
 // Sort {{{
 
 /*
-function init_sort_combobox() {
-    $("#sort_combobox").multiselect({
-       multiple: false,
-       header: sort_select_label,
-       noneSelectedText: sort_select_label,
-       selectedList: 1,
-       click: function(event, ui){
-            $(this).multiselect("close");
-            cookie(sort_cookie_name, ui.value);
-alert(ui.value);
-            window.location.reload();
-       }
-    });
-}
-*/
+   function init_sort_combobox() {
+   $("#sort_combobox").multiselect({
+   multiple: false,
+   header: sort_select_label,
+   noneSelectedText: sort_select_label,
+   selectedList: 1,
+   click: function(event, ui){
+   $(this).multiselect("close");
+   cookie(sort_cookie_name, ui.value);
+   alert(ui.value);
+   window.location.reload();
+   }
+   });
+   }
+   */
 
 // }}}
 
 function init() {
-    $("#calibre-home-link").click(function() { window.location = "http://calibre-ebook.com"; });
-
     //init_sort_combobox();
-
     $("#search_box input:submit").button();
 }
 
@@ -142,81 +171,81 @@ function render_error(msg) {
 // Category feed {{{
 
 function category_clicked() {
-   var href = $(this).find("a").attr('href');
-   window.location = href;
+    var href = $(this).find("a").attr('href');
+    window.location = href;
 }
 
 function category() {  
-	var $g = $("#groups");
+    var $g = $("#groups");
 
-	$(".category .category-item").click(category_clicked);
+    $(".category .category-item").click(category_clicked);
 
-	$(".category a.navlink").button();
+    $(".category a.navlink").button();
 
 
-	//$g.find(".loading,.loaded").addClass("hide");
-	$g.find("h3").each(function() {
-		$(this).addClass("accordion-heading").button();
-		$(this).next("div").addClass("accordion-inner").collapse('hide');
-			$(this).on("click", function(event, ui) {
-				var $t = $(this),
-				$c = $t.next("div");
-				$c.collapse('toggle');
-				var href = $t.find("a.load_href").attr('href');
-				if (href) {
-					$.ajax({
-						url:href,
-						cache: false,
-						data:{'sort':cookie(sort_cookie_name)},
-						success: function(data) {
-							$c.find(".loaded").html(data).removeClass('hide').show();
-							$c.find(".loading").addClass('hide');
-						}
-					});
-				}
-				
-			
-				event.preventDefault();
-				return false;
+    //$g.find(".loading,.loaded").addClass("hide");
+    $g.find("h3").each(function() {
+        $(this).addClass("accordion-heading").button();
+        $(this).next("div").addClass("accordion-inner").collapse('hide');
+        $(this).on("click", function(event, ui) {
+            var $t = $(this),
+            $c = $t.next("div");
+        $c.collapse('toggle');
+        var href = $t.find("a.load_href").attr('href');
+        if (href) {
+            $.ajax({
+                url:href,
+                cache: false,
+                data:{'sort':cookie(sort_cookie_name)},
+                success: function(data) {
+                    $c.find(".loaded").html(data).removeClass('hide').show();
+                    $c.find(".loading").addClass('hide');
+                }
+            });
+        }
 
-				});
-			});
-	/*
-	   $("#groups").accordion({
-collapsible: true,
-active: false,
-autoHeight: false,
-clearStyle: true,
-header: "h3",
 
-change: function(event, ui) {
-if (ui.newContent) {
-var href = ui.newContent.prev().children("a.load_href").attr('href');
-ui.newContent.children(".loading").show();
-if (href) {
-$.ajax({
-url:href,
-cache: false,
-data:{'sort':cookie(sort_cookie_name)},
-success: function(data) {
-this.children(".loaded").html(data);
-this.children(".loaded").show();
-this.children(".loading").hide();
-this.find('.category-item').click(category_clicked);
-},
-context: ui.newContent,
-dataType: "json",
-timeout: 600000, 
-error: function(xhr, stat, err) {
-this.children(".loaded").html(render_error(stat));
-this.children(".loaded").show();
-this.children(".loading").hide();
-}
-});
-}
-}
-}
-});*/
+        event.preventDefault();
+        return false;
+
+        });
+    });
+    /*
+       $("#groups").accordion({
+       collapsible: true,
+       active: false,
+       autoHeight: false,
+       clearStyle: true,
+       header: "h3",
+
+       change: function(event, ui) {
+       if (ui.newContent) {
+       var href = ui.newContent.prev().children("a.load_href").attr('href');
+       ui.newContent.children(".loading").show();
+       if (href) {
+       $.ajax({
+       url:href,
+       cache: false,
+       data:{'sort':cookie(sort_cookie_name)},
+       success: function(data) {
+       this.children(".loaded").html(data);
+       this.children(".loaded").show();
+       this.children(".loading").hide();
+       this.find('.category-item').click(category_clicked);
+       },
+       context: ui.newContent,
+       dataType: "json",
+       timeout: 600000, 
+       error: function(xhr, stat, err) {
+       this.children(".loaded").html(render_error(stat));
+       this.children(".loaded").show();
+       this.children(".loading").hide();
+       }
+       });
+       }
+       }
+       }
+       });*/
 }
 // }}}
 
@@ -242,94 +271,24 @@ function previous_page() {
     else last_page();
 }
 
-function gp_internal(id) {
-    //var gp = $('#goto_page_dialog');
-    //gp.modal('hide');
-    var elem = $("#booklist #" + id);
-    load_page(elem);
-}
-
-// this actually just builds the dropdown now... disliked a modal for it.
-function goto_page() {
-	var pl = $('#booklist > #pagelist');
-	var dd = $(".navmiddle").find("select");
-	var i = 0;
-	var cur = $(".page:visible").eq(0).attr("id");
-	if (dd.length) {
-		dd.find("[value='"+cur+"']").attr('selected',true);
-		return;
-	}
-	var html="<select onchange='gp_internal(this.value);return false;'>";
-	pl.find("li").each(function() {
-		var $link = $(this),
-		title = $link.attr("title"),
-		id = "page"+i;
-		text = $link.text();
-		
-		if (id === cur) {
-			html += "<option SELECTED value='"+id+"'>"+text+"</option>";
-		} else {
-			html += "<option value='"+id+"'>"+text+"</option>";
-		}
-		i++;
-	});
-	$(".navmiddle").children().hide();
-	$(".navmiddle").prepend(html);
-}
 function goto_page_old() {
     var gp = $('#goto_page_dialog');
     var gp_body = gp.find(".modal-body");
     var pl = $('#booklist > #pagelist');
     pl.find("li").each(function() {
-	alert($(this).find("a").attr("title"));
+        alert($(this).find("a").attr("title"));
     });
     gp.find(".modal-title").html(pl.attr('title'));
     gp_body.html(pl.html());
-/*
-    gp.dialog('option', 'title', pl.attr('title'));
-    gp.dialog('option', 'height', $(window).height() - 100);
-    gp.dialog('open');
-*/
-	gp.modal('show');
+    /*
+       gp.dialog('option', 'title', pl.attr('title'));
+       gp.dialog('option', 'height', $(window).height() - 100);
+       gp.dialog('open');
+       */
+    gp.modal('show');
 
 }
 
-function load_page(elem) {
-    if (elem.is(":visible")) return;
-    var ld = elem.find('.load_data');
-    var ids = ld.attr('title');
-    var href = ld.find(".url").attr('title');
-    elem.children(".loaded").hide();
-
-    $.ajax({
-        url: href,
-        context: elem,
-        dataType: "json",
-        cache : false,
-        type: 'POST',
-        timeout: 600000, //milliseconds (10 minutes)
-        data: {'ids': ids},
-        error: function(xhr, stat, err) {
-            this.children(".loaded").html(render_error(stat));
-            this.children(".loaded").show();
-            this.children(".loading").hide();
-        },
-        success: function(data) {
-            this.children(".loaded").html(data);
-            this.find(".left a.read").button();
-            this.children(".loading").hide();
-            this.parent().find('.navmiddle .start').html(this.find('.load_data .start').attr('title'));
-            this.parent().find('.navmiddle .end').html(this.find('.load_data .end').attr('title'));
-            this.children(".loaded").fadeIn(1000);
-	    goto_page();
-    $('article').readmore();
-        }
-    });
-    $("#booklist .page:visible").hide();
-    elem.children(".loaded").hide();
-    elem.children(".loading").show();
-    elem.show();
-}
 
 function hidesort() {$("#content > .sort_select").hide();}
 
@@ -340,18 +299,18 @@ function booklist(hide_sort) {
         $("#booklist").html(render_error("No books found"));
         return;
     }
-/*
-    $("#book_details_dialog").dialog({
-        autoOpen: false,
-        modal: true,
-        show: 'slide'
-    });
-    $("#goto_page_dialog").dialog({
-        autoOpen: false,
-        modal: true,
-        show: 'slide'
-    });
-*/
+    /*
+       $("#book_details_dialog").dialog({
+       autoOpen: false,
+       modal: true,
+       show: 'slide'
+       });
+       $("#goto_page_dialog").dialog({
+       autoOpen: false,
+       modal: true,
+       show: 'slide'
+       });
+       */
 
     first_page(); 
 }
@@ -371,12 +330,12 @@ function show_details(a_dom) {
     var bd_body = bd.find(".modal-body");
     var bd_title = $("#modal_book_title");
     bd_title.html(book.find(".title").text());
-	bd.modal('show');
+    bd.modal('show');
     bd_body.html('<span class="loading"><img src="'+url_prefix+'/static/loading.gif" alt="Loading" />Loading, please wait&hellip;</span>');
     /*bd.dialog('option', 'width', $(window).width() - 100);
-    bd.dialog('option', 'height', $(window).height() - 100);
-    bd.dialog('option', 'title', book.find('.title').text());
-*/
+      bd.dialog('option', 'height', $(window).height() - 100);
+      bd.dialog('option', 'title', book.find('.title').text());
+      */
 
     $.ajax({
         url: book.find('.details-href').attr('title'),
@@ -412,20 +371,99 @@ function book() {
         $('#main').height(height);
     });
 }
-
-$(document).ready(function() {
-
-$(".ui-button").button().addClass("btn");
-$(".navlink, .navleft a, .navright a").button().addClass("btn btn-primary btn-small");
-goto_page();
-if (window.location.href.indexOf('category') !== -1) 
-	if (window.location.href.indexOf('newest') === -1) 
-		$("#sort_combobox_container").removeClass("hide");
-
-$("#sort_combobox").change(function() {
-            cookie(sort_cookie_name, this.value);
-            window.location.reload();
 });
 
 
-});
+/* This has to live outside ready() because it's called inline via the python binary */
+function first_page() {
+    load_page($("#booklist #page0"));
+}
+
+function last_page() {
+    load_page($("#booklist .page").last());
+}
+
+function next_page() {
+    var elem = $("#booklist .page:visible").next('.page');
+    if (elem.length > 0) load_page(elem);
+    else first_page();
+}
+
+function previous_page() {
+    var elem = $("#booklist .page:visible").prev('.page');
+    if (elem.length > 0) load_page(elem);
+    else last_page();
+}
+
+function load_page(elem) {
+    if (elem.is(":visible")) return;
+    var ld = elem.find('.load_data');
+    var ids = ld.attr('title');
+    var href = ld.find(".url").attr('title');
+    elem.children(".loaded").hide();
+
+    $.ajax({
+        url: href,
+        context: elem,
+        dataType: "json",
+        cache : false,
+        type: 'POST',
+        timeout: 600000, //milliseconds (10 minutes)
+        data: {'ids': ids},
+        error: function(xhr, stat, err) {
+            this.children(".loaded").html(render_error(stat));
+            this.children(".loaded").show();
+            this.children(".loading").hide();
+        },
+        success: function(data) {
+            this.children(".loaded").html(data);
+            this.find(".left a.read").button();
+            this.children(".loading").hide();
+            this.parent().find('.navmiddle .start').html(this.find('.load_data .start').attr('title'));
+            this.parent().find('.navmiddle .end').html(this.find('.load_data .end').attr('title'));
+            this.children(".loaded").fadeIn(1000);
+            goto_page();
+            $('article').readmore();
+        }
+    });
+    $("#booklist .page:visible").hide();
+    elem.children(".loaded").hide();
+    elem.children(".loading").show();
+    elem.show();
+}
+function gp_internal(id) {
+    //var gp = $('#goto_page_dialog');
+    //gp.modal('hide');
+    var elem = $("#booklist #" + id);
+    load_page(elem);
+}
+
+// this actually just builds the dropdown now... disliked a modal for it.
+function goto_page() {
+    var pl = $('#booklist > #pagelist');
+    var dd = $(".navmiddle").find("select");
+    var i = 0;
+    var cur = $(".page:visible").eq(0).attr("id");
+    if (dd.length) {
+        dd.find("[value='"+cur+"']").attr('selected',true);
+        return;
+    }
+    var html="<select onchange='gp_internal(this.value);return false;'>";
+    pl.find("li").each(function() {
+        var $link = $(this),
+        title = $link.attr("title"),
+        id = "page"+i;
+    text = $link.text();
+
+    if (id === cur) {
+        html += "<option SELECTED value='"+id+"'>"+text+"</option>";
+    } else {
+        html += "<option value='"+id+"'>"+text+"</option>";
+    }
+    i++;
+    });
+    $(".navmiddle").children().hide();
+    $(".navmiddle").prepend(html);
+}
+
+
